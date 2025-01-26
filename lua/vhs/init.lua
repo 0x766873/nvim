40a56d4c3a5a1c5e440c326124fbb60fc1e7260a
vhs <vhs@Atlas.local.sec>
2025-01-26 09:56:16 -0500

    Initial commit

---
+++ lua/vhs/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " " -- Set leader key before lazy
+
+require("vhs.lazy_init")
+require("vhs.remap")
+require("vhs.set")
---
+++ lua/vhs/set.lua
@@ -0,0 +1,16 @@
+vim.cmd.colorscheme("tokyonight")
+
+vim.opt.nu = true                 -- set line numbers -- set line numbers
+ 
+-- set tab size to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+ 
+vim.opt.incsearch = true -- incremental search
+ 
+vim.opt.termguicolors = true
